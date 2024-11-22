794d4002e5b65d4aaf53c1650e83fc99ef4322af
Teerachai Laothong <teerachai@finsoft2023.com>
2024-11-22 14:41:48 +0700

    remove unused configuration and component files

--- src/apps/facingweb/app/pages/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <UContainer>
-    <div class="flex items-center justify-center h-screen">
-      <img src="/org-logo.png" alt="Fin Soft powered KIOSK" class="w-32 h-32" />
-    </div>
-  </UContainer>
-</template>

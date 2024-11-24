f4e3b73ece4e15081760f0797e674cc57a450b44
Teerachai Laothong <teerachai@finsoft2023.com>
2024-11-24 16:38:47 +0700

    Set application to fullscreen and disable window frame in kiosk mode

--- src/apps/kiosk-q/src/main.ts
+++ src/apps/kiosk-q/src/main.ts
@@ -14,6 +14,7 @@ const createWindow = () => {
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
+    frame: false,
   });
 
   const KIOSK_SERVER_URL = process.env.KIOSK_SERVER_URL;
@@ -31,6 +32,8 @@ const createWindow = () => {
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools();
+
+  mainWindow.setFullScreen(true);
 };
 
 // This method will be called when Electron has finished
